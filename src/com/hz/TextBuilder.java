a73fdd3d1211769d1a0c2a60f0cd242ef1ee83b0
userxperience <tijnvisser3@gmail.com>
2023-02-18 15:01:05 +0100

    added template method for builders

--- src/com/hz/TextBuilder.java
+++ src/com/hz/TextBuilder.java
@@ -1,34 +1,24 @@
 package com.hz;
 
-public class TextBuilder implements builder { //a builder to seperate lines of text into a more readable format.
-   String _text = "";
+public abstract class TextBuilder {
 
-   @Override
-   public void paragraph1(String content) {
-      _text += "\n" + content;
-   }
+    protected String _text;
 
-   @Override
-   public void paragraph2(String content) {
-      _text += "\n" + content;
-   }
+    protected void paragraph1(String content) {
+        _text += "\n" + content;
+    }
 
-   @Override
-   public void paragraph3(String content) {
-      _text += "\n" + content;
-   }
+    protected void paragraph2(String content) {
+        _text += "\n" + content;
+    }
 
-   @Override
-   public void paragraph4(String content) {
-      _text += "\n \n" + content;
-   }
+    protected void paragraph3(String content) {
+        _text += "\n" + content;
+    }
 
-   String getText(String para1, String para2, String para3, String para4) {
-      this.paragraph1(para1);
-      this.paragraph2(para2);
-      this.paragraph3(para3);
-      this.paragraph4(para4);
-      return this._text;
-   }
+    protected void paragraph4(String content) {
+        _text += "\n" + content;
+    }
 
+    abstract String getText(String para1, String para2, String para3, String para4);
 }
